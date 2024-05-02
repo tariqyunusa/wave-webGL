481d6a2e571c80b44c9a0b1e91b562caca089d66
Tariq Yunusa <tariikhyunusa@gmail.com>
2024-05-02 22:39:15 +0100

    change

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,22 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+import '../app/styles.css'
+import { Canvas } from '@react-three/fiber'
+import {planeBufferGeometry} from "three"
+
+
+const Scene = () => {
+  return (
+    <Canvas>
+      <mesh>
+        <planeBufferGeometry args={[3, 5]}/>
+        <meshBasicMaterial color="red" /> {/* Adding a basic material */}
+      </mesh>
+    </Canvas>
+  )
+}
 
 export default function Home() {
   return (
-    <div></div>
+    <Scene />
   );
 }
