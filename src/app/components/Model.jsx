c808ef4804cac787f3b3f3081fe9bf61818cfab2
Tariq Yunusa <tariikhyunusa@gmail.com>
2024-05-06 08:15:01 +0100

    fixed the uniform update(uTime)

--- src/app/components/Model.jsx
+++ src/app/components/Model.jsx
@@ -1,16 +1,31 @@
+import { useEffect, useRef, useState } from 'react'
 import React from 'react'
 import { vertex, fragment } from '../components/Shader'
+import { useFrame } from '@react-three/fiber'
+import { waveShaderMaterial } from '../components/Shader'
+
 
 
 export default function model() {
+  const waveRef = useRef()
+  useFrame(({ clock }) => {
+    if (waveRef.current) {
+      waveRef.current.uniforms.uTime.value = clock.getElapsedTime();
+    }
+  });
+
+
   return (
+   
     <mesh>
       <planeGeometry args={[3, 3, 15, 15]}/>
       {/* <meshBasicMaterial color={"red"} wireframe={true}/> */}
       <shaderMaterial
+      ref={waveRef}
        vertexShader={vertex}
        fragmentShader={fragment}
        wireframe={true}
+      uniforms = {{uTime: {value: 0.0}}}
        />
     </mesh>
   )
--- src/app/components/Scene.jsx
+++ src/app/components/Scene.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import Model from '../components/Model'
 import { Canvas } from '@react-three/fiber'
 
 export default function Scene() {
   return (
     <Canvas>
+      <Suspense fallback={null}>
       <Model />
+      </Suspense>
+     
     </Canvas>
   )
 }
